e7b3545ed4b1521fb227cf307530d4837922f0d2
melanysosa <sosamelanynoelia@gmail.com>
2023-10-02 16:33:56 -0300

    Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
-import './globals.css';
 import { Montserrat } from 'next/font/google';
+import Footer from '../components/Footer';
+import './globals.css';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='es'>
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
