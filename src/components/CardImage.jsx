b502e9e67443240d72e71d5afdf8047cf5776746
melanysosa <sosamelanynoelia@gmail.com>
2023-10-24 21:54:12 -0300

    home

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -1,35 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
-import BtnAdd from './buttons/BtnAdd';
 
 function CardImage({
   src,
   imageClassName,
-  title,
-  subTitle,
   alt,
-  on,
   handleClick,
-  titleImage,
+  category,
+  onCategory,
 }) {
   return (
-    <div className='relative '>
-      <div className='group' onClick={handleClick}>
-        <Image
-          alt={alt}
-          src={src}
-          height={427}
-          width={956}
-          className={imageClassName}
-        />
-        <div className='absolute inset-0 flex items-center justify-center text-white md:text-xl font-bold'>
-          {titleImage}
+    <div>
+      <div className='relative '>
+        <div onClick={handleClick}>
+          <Image
+            alt={alt}
+            src={src}
+            height={427}
+            width={956}
+            className={imageClassName}
+          />
+          {onCategory && (
+            <div className='absolute inset-0  flex items-center md:h-60 md:hover:bg-violet-400/75 justify-center bg-white/40 text-black md:text-xl font-bold '>
+              {category}
+            </div>
+          )}
         </div>
-      </div>
-      <p className='text-sm md:text-lg font-extrabold mt-2'>{title}</p>
-      <p className='text-sm md:text-base font-bold m-2'>{subTitle}</p>
-      <div className='flex justify-center items-center mt-4 mb-4'>
-        {on && <BtnAdd />}
       </div>
     </div>
   );
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,12 +4,14 @@ import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import CardImage from './CardImage';
+import ProductCard from './ProductCard';
 
 function CarouselHome() {
   const apiUrl = 'https://source.unsplash.com/';
 
   const [slides, setSlides] = useState([]);
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleSideImageClick = index => {
     setSelectedImageIndex(index);
@@ -21,74 +23,82 @@ function CarouselHome() {
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < 4; i++) {
       imagePromises.push(
-        fetch(`${apiUrl}956x427/?fashion`).then(response => response.url),
+        fetch(`${apiUrl}956x427/?fashion`).then(response => ({
+          url: response.url,
+        })),
       );
     }
 
     const imageUrls = await Promise.all(imagePromises);
     setSlides(imageUrls);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchImages();
   }, []);
 
-  if (slides.length === 0) {
-    return null;
-  }
-
   return (
-    <div className='relative z-10 '>
-      <Carousel
-        showArrows={true}
-        centerMode={true}
-        selectedItem={selectedImageIndex}
-        emulateTouch={true}
-        showThumbs={false}
-        dynamicHeight={true}
-        showStatus={false}
-        infiniteLoop={true}
-        autoPlay={true}
-        interval={5000}>
-        {slides.map((slide, index) => (
-          <CardImage
-            handleClick={() => handleSideImageClick(index)}
-            key={index}
-            src={slide}
-            alt={`url(${slide})`}
-            imageClassName='h-[160px] w-[280px] sm:w-full sm:h-full object-cover  px-2 xl:px-6'
-          />
-        ))}
-      </Carousel>
-      <div className='grid md:grid-cols-4 xl:mx-52 sm:mx-20 grid-cols-2 gap-4 p-2 mt-10'>
-        {slides.map((slide, index) => (
-          <CardImage
-            titleImage={` Categoria ${index + 1}`}
-            handleClick={() => handleSideImageClick(index)}
-            key={index}
-            src={slide}
-            alt={`url(${slide})`}
-            imageClassName={`object-cover text-white flex items-center justify-center font-semibold w-full h-32 xl:h-60 bg-cover ring-2 ${
-              index === selectedImageIndex
-                ? 'ring-purple-500'
-                : 'ring-transparent'
-            }`}
-          />
-        ))}
-      </div>
-      <div className='grid md:grid-cols-4 xl:mx-52 sm:mx-20 grid-cols-2 gap-4 p-2 mt-10'>
-        {slides.map((slide, index) => (
-          <CardImage
-            key={index}
-            src={slide}
-            alt={`url(${slide})`}
-            title='Nombre del producto'
-            subTitle='Precio'
-            on={true}
-            imageClassName='w-[160px] h-[125px] md:w-[231px] md:h-[221px] object-cover'
-          />
-        ))}
-      </div>
+    <div className='relative z-10'>
+      {isLoading ? (
+        <p className='flex justify-center font-bold text-2xl mb-10 '>
+          Loading...
+        </p>
+      ) : (
+        <div>
+          <Carousel
+            showArrows={true}
+            centerMode={true}
+            selectedItem={selectedImageIndex}
+            emulateTouch={true}
+            showThumbs={false}
+            dynamicHeight={true}
+            showStatus={false}
+            infiniteLoop={true}
+            autoPlay={true}
+            interval={5000}>
+            {slides.map((slide, index) => (
+              <CardImage
+                handleClick={() => handleSideImageClick(index)}
+                key={index}
+                src={slide.url}
+                alt={`url(${slide.url})`}
+                imageClassName='h-[160px] w-[280px] sm:w-full sm:h-full object-cover  px-2 xl:px-6'
+              />
+            ))}
+          </Carousel>
+          <div className='grid md:grid-cols-4 xl:mx-40 sm:mx-20 grid-cols-2 gap-4 p-2 mt-10'>
+            {slides.map((slide, index) => (
+              <CardImage
+                key={index}
+                src={slide.url}
+                alt={`url(${slide.url})`}
+                onCategory={true}
+                category={` Categoria ${index + 1}`}
+                handleClick={() => handleSideImageClick(index)}
+                imageClassName={`object-cover text-white flex items-center justify-center font-semibold w-full h-32 md:h-60 bg-cover ring-4  ${
+                  index === selectedImageIndex
+                    ? 'ring-purple-500'
+                    : 'ring-transparent'
+                }`}
+              />
+            ))}
+          </div>
+          <p className='flex font-bold text-xl sm:text-2xl justify-center items-center uppercase my-8'>
+            Productos destacados
+          </p>
+          <div className='grid md:grid-cols-4 xl:mx-40 sm:mx-20 grid-cols-2 gap-4 p-2 mt-10'>
+            {slides.map((slide, index) => (
+              <ProductCard
+                key={index}
+                image={slide.url}
+                title='Nombre del producto'
+                price='Precio'
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
